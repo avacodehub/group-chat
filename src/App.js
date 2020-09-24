59880e77ce1419de0087cb46e077b7fddfecf189
avacodehub <avacodehub@gmail.com>
2020-09-24 19:11:52 +0300

    styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
 import firebase from "firebase/app";
@@ -22,86 +22,108 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>FastChat</h1>
-        <SignOut />
-      </header>
+    <>
+      {user ? (
+        <div className="App">
+          <header className="App-header">
+            <h3>Group Chat</h3>
+            <SignOut />
+          </header>
 
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-    </div>
+          <section>
+            <ChatRoom />
+          </section>
+        </div>
+      ) : (
+        <SignIn />
+      )}
+    </>
   );
 }
 
 const SignIn = () => {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    auth.signInWithPopup(provider);
   };
   return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <>
+      <div className="signin">
+        <div>
+          <h1>Try our new realtime chat! </h1>
+          {/* <h2>Instant sign in with your Google account</h2> */}
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 const SignOut = () => {
-  return auth.currentUser && (
-    <button onClick={()=> auth.signOut()}>Sign Out</button>
-  )
-}
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+};
 
 const ChatRoom = () => {
-  const messagesRef = firestore.collection('messages')
-  const query = messagesRef.orderBy('createdAt').limit(25)
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
 
-  const [messages] = useCollectionData(query, {idField: 'id'})
-  const [formValue, setFormValue] = useState('')
-  const scrollPoint = useRef()
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
+  const scrollPoint = useRef();
 
-  const sendMessage = async(e) => {
-    e.preventDefault()
-    const {uid, photoURL} = auth.currentUser
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL, displayName } = auth.currentUser;
     await messagesRef.add({
+      displayName: displayName,
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
-    setFormValue('')
-    scrollPoint.current.scrollIntoView({behavior: "smooth"})
-  }
+    setFormValue("");
+    scrollPoint.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <>
-    <main>
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-      <div ref={scrollPoint}></div>
-    </main>
-    <form onSubmit={sendMessage}>
-      <input value={formValue} onChange={(e)=>setFormValue(e.target.value)}/>
-      <button type="submit">Send</button>
-    </form>
+        <div ref={scrollPoint}></div>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
 const ChatMessage = (props) => {
-  const {text, uid, photoURL} = props.message
+  const { displayName, text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
   return (
     <div className={`message ${messageClass}`}>
-      <img src={photoURL} alt={'avatar'}/>
-      <p>{text}</p>
+      <img src={photoURL} alt={"avatar"} />
+      <div>
+        <p className="username">{displayName}</p>
+        <p>{text}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
